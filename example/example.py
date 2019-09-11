c261030719f8fe034b99744ad8374c730decd168
Fakhir Khan <thefakhir@gmail.com>
2019-09-11 16:47:43 +0500

    example updated with class

--- example/example.py
+++ example/example.py
@@ -7,10 +7,8 @@ def test():
     default_values = {'Datasets': ['VOC', 'COCO'],
                       'Model': ['MODEL_PATH'],
                       'RESULT': ['RESULT_FOLDER_PATH', 'DATASET_TRAIN_CSV', 'DATASET_TEST_CSV']}
-    input_params = {'config_file': 'config.ini',
-                    'get_new_config': True,
-                    'default_keys': default_values}
-    config = ConfigParser(input_parameters=input_params)
+
+    config = ConfigParser(config_file='config.ini', get_new_config=True, default_keys=default_values)
     config.create_config()
     config_after_reading = config.read_config()
     print(config_after_reading)
